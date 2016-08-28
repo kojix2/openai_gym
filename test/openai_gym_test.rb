081b2e2f19d97d07802bcc8e0e3d310a1c038321
kojix2 <kojix2@users.noreply.github.com>
2016-08-28 23:50:27 +0900

    test test

--- test/openai_gym_test.rb
+++ test/openai_gym_test.rb
@@ -1,11 +1,12 @@
 require 'test_helper'
 
 class OpenaiGymTest < Minitest::Test
+  def setup
+    @env = OpenAI::Gym.new("Pong-v0")
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::OpenaiGym::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
